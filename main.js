3bc9110c3af36d7e807f873bc59d383dcbb430ae
Kushagra Mehta <Kushagra8@users.noreply.github.com>
2022-12-20 10:01:32 -0500

    Update main.js
    api update

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 /* this is the javascript behind our app */
 // API call
 // $(document).ready(function() {
-//   $.getJSON("http://api.openweathermap.org/data/2.5/weather?q=" + city + "uk&units=metric&appid=48a8c111df81fd58c7240c8f432660e7").then(function(data) {
+//   $.getJSON("http://api.openweathermap.org/data/2.5/forecast?id=524901&appid={API key}" + city + "uk&units=metric&appid=48a8c111df81fd58c7240c8f432660e7").then(function(data) {
 //     console.log(data);
 //     var weather = document.getElementById('weather');
 //     weather.innerText = data.main.temp;
